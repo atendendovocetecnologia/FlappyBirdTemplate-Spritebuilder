ec282aaa6ec943abe274de57c0b85c8c05073906
Nilton Leandro Souza Xavier <NiltonLeandro@MacBook-Air-de-Nilton.local>
2015-07-23 10:17:06 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -14,10 +14,11 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    [character flap];
 }
 
 // put new methods here
-(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
+-(void)touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event {
     // this will get called every time the player touches the screen
     [character flap];
 }
